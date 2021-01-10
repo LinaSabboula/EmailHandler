674bb897f151558c09ae408f159ac03f3838093a
Lina <linamuhab@gmail.com>
2021-01-10 17:52:34 +0200

    Added dictionary to store relevant information associated with sender

--- Handler.py
+++ Handler.py
@@ -11,12 +11,14 @@ server = IMAPClient('imap.gmail.com', use_uid=True, ssl=True)
 username = input("Username: ")
 try:
     server.login(username, getpass.getpass(prompt="Password: "))
+    # server.login(username, password)
     print("Authentication Success")
     folder = server.select_folder('INBOX')
     print("%d messages in INBOX "%folder[b'EXISTS'])
     try:
         messages = server.search('ON 8-JAN-2021')
         # messages = server.search('ALL')
+        dictionary = {}
         for uid, message_data in server.fetch(messages, ['ENVELOPE']).items():
             envelope = message_data[b'ENVELOPE']
             address = envelope.from_[0]
@@ -24,10 +26,19 @@ try:
 
             sender_mail = str(address.mailbox, 'utf-8') + "@" + str(address.host, 'utf-8')
             subject = email.header.decode_header(str(envelope.subject, 'utf-8'))[0][0]
-            date = envelope.date
+            email_date = envelope.date
             if isinstance(subject, bytes):
                 subject = str(subject, 'utf-8')
-            print(sender_name, sender_mail, subject)
+            if sender_mail not in dictionary:
+                # key: sender email
+                # values: [sender name, email count, oldest date from sender, newest date from sender]
+                dictionary[sender_mail] = [sender_name, 1, email_date, email_date]
+            else:
+                dictionary[sender_mail][1] += 1 # increase email count
+                if dictionary[sender_mail][2] > email_date: # adjusting oldest date
+                    dictionary[sender_mail][2] = email_date
+                if dictionary[sender_mail][3] < email_date: #adjusting newest date
+                    dictionary[sender_mail][3] = email_date
 
     except IMAPClient.Error as e:
         print(e)
