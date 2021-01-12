cad2fa56effdaed5e388d2e96e37c1645f410fdf
Lina <linamuhab@gmail.com>
2021-01-12 19:13:33 +0200

    Fixed encoding for names and added subject to CSV

--- Handler.py
+++ Handler.py
@@ -19,39 +19,47 @@ try:
     try:
         # messages = server.search('ON 8-JAN-2021')
         messages = server.search('ALL')
+        # messages = server.search(['FROM', 'notify@twitter.com'])
         dictionary = {}
         for uid, message_data in server.fetch(messages, ['ENVELOPE']).items():
             envelope = message_data[b'ENVELOPE']
             email_date = envelope.date
             address = envelope.from_[0]
             sender_mail = str(address.mailbox, 'utf-8') + "@" + str(address.host, 'utf-8')
-            if address.name != None:
-                sender_name = str(address.name, 'utf-8')
+            if address.name != None: #Some older emails wouldn't have a name property
+                # sender_name = str(address.name, 'utf-8')
+                sender_name = email.header.decode_header(str(address.name, 'utf-8'))[0][0]
+                if isinstance(sender_name, bytes):
+                    sender_name = str(sender_name, 'utf-8')
             else:
                 sender_name = sender_mail
+
             if envelope.subject != None:
                 subject = email.header.decode_header(str(envelope.subject, 'utf-8'))[0][0]
-
                 if isinstance(subject, bytes):
                     subject = str(subject, 'utf-8')
+            else:
+                subject = 'None'
             if sender_mail not in dictionary:
                 # key: sender email
                 # values: [sender name, email count, oldest date from sender, newest date from sender]
-                dictionary[sender_mail] = [sender_name, 1, email_date, email_date]
+                dictionary[sender_mail] = [sender_name, 1, email_date, email_date,[subject]]
             else:
                 dictionary[sender_mail][1] += 1 # increase email count
+                dictionary[sender_mail][4].append(subject)
                 if dictionary[sender_mail][2] > email_date: # adjusting oldest date
                     dictionary[sender_mail][2] = email_date
                 if dictionary[sender_mail][3] < email_date: #adjusting newest date
                     dictionary[sender_mail][3] = email_date
 
+
         now = datetime.datetime.now().strftime("%d-%m-%Y")
         file_name = "email_reports " + now + ".csv"
-        with open(file_name, mode='w', newline="") as csvfile:
+        with open(file_name, mode='w', newline="", encoding='utf-8-sig') as csvfile:
             writer = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["Name", "Email", "Count", "Oldest Email", "Newest Email"])
+            writer.writerow(["Name", "Email", "Count", "Oldest Email", "Newest Email","Subjects"])
             for key, value in dictionary.items():
-                writer.writerow([value[0],key, value[1], value[2], value[3]])
+                writer.writerow([value[0],key, value[1], value[2], value[3], value[4]])
 
 
     except IMAPClient.Error as e:
