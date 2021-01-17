b449d3f10a5d174a29f34cb9736ff6d3ee300414
Lina <linamuhab@gmail.com>
2021-01-17 21:02:43 +0200

    Added read_csv function which reads row flags specified for deletion 'Delete' column indicated whether to delete all emails from this address

--- Handler.py
+++ Handler.py
@@ -4,7 +4,7 @@ import email
 import csv
 from imapclient import IMAPClient
 import datetime
-
+from itertools import chain
 
 imaplib._MAXLINE = 1000000
 class EmailHandler:
@@ -44,19 +44,23 @@ class EmailHandler:
                     a search term to retrieve specific email messages that satisfy term
         """
         try:
-            print("Fetching messages..")
-            self.messages = self.server.search(search_term)
+            if len(search_term) > 1:
+                print("Fetching messages from %s.."%search_term[1])
+            else:
+                print("Fetching messages...")
+            messages = self.server.search(search_term)
+            return messages
         except IMAPClient.Error as e:
             print(e)
 
-    def get_messages(self):
+    def get_messages(self, messages):
         """
 
         :return:
         """
         print("Formatting messages..")
         self.dictionary = {}
-        for uid, message_data in self.server.fetch(self.messages, ['ENVELOPE']).items():
+        for uid, message_data in self.server.fetch(messages, ['ENVELOPE']).items():
             envelope = message_data[b'ENVELOPE']
             email_date = envelope.date
             address = envelope.from_[0]
@@ -78,14 +82,15 @@ class EmailHandler:
             if sender_mail not in self.dictionary:
                 # key: sender email
                 # values: [sender name, email count, oldest date from sender, newest date from sender]
-                self.dictionary[sender_mail] = [sender_name, 1, email_date, email_date,[subject]]
+                self.dictionary[sender_mail] = [sender_name, 1, email_date, email_date,subject]
             else:
                 self.dictionary[sender_mail][1] += 1 # increase email count
-                self.dictionary[sender_mail][4].append(subject)
+                # self.dictionary[sender_mail][4].append(subject)
                 if self.dictionary[sender_mail][2] > email_date: # adjusting oldest date
                     self.dictionary[sender_mail][2] = email_date
                 if self.dictionary[sender_mail][3] < email_date: #adjusting newest date
                     self.dictionary[sender_mail][3] = email_date
+
 
 
     def create_csv(self, report_name):
@@ -103,16 +108,57 @@ class EmailHandler:
             for key, value in self.dictionary.items():
                 writer.writerow([value[0],key, value[1], value[2], value[3], value[4]])
 
+    def read_csv(self, file):
+        """
 
+        :param file:
+        :return:
+        """
+        print("reading csv")
+        UIDs = []
+        with open(file, mode='r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['delete'] == "1":
+                    address = row['Email']
+                    UIDs.append((self.search(["FROM", address])))
+        UIDs = list(chain.from_iterable(UIDs))
+        return UIDs
 
 
 
 handler = EmailHandler()
 handler.login(username = input("Username: "), password=getpass.getpass(prompt='Password: '))
 # handler.login(username, password)
+
+# search_term1 = ['FROM', 'notify@twitter.com']
+# search_term2 = ['SINCE', datetime.date(2019, 6, 1)]
+
+# search_result1 = handler.search(search_term1)
+# search_result2 = handler.search(search_term2)
+# res = list(set(search_result1).intersection(search_result2))
+# print(set(res1).intersection(res2))
+# print(res)
+
+# ========================================
+
+
 handler.open_folder("inbox")
-search_term = 'ALL'
-handler.search([search_term])
-handler.get_messages()
-handler.create_csv("email_reports ")
+create_report = input("Create Report Y/N:  ")
+if create_report.lower() == "y":
+    search_term = ['ALL']
+    search_result = handler.search(search_term)
+    if len(search_result) != 0:
+        handler.get_messages(search_result)
+        handler.create_csv("email_reports ")
+    else:
+        print("No messages")
+delete_file = 'delete_report.csv'
+read_csv = input("Would you like to read csv specified for deletion?")
+if read_csv.lower() == 'y':
+    UIDs = handler.read_csv(delete_file)
+    delete = input("%d emails selected for deletion, would you like to proceed?" %len(UIDs))
+    if delete.lower() == 'y':
+        pass
+
 
